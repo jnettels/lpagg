d14afd15eb52d5e7e42d9e758867cfab32be8415
Joris Zimmermann <joris.nettelstroth@stw.de>
2026-08-14 16:37:49 +0200

    Switch to hardcoded version name

--- src/lpagg/__init__.py
+++ src/lpagg/__init__.py
@@ -1,15 +1,3 @@
 """Load profile aggregator for building simulations."""
-from importlib.metadata import version, PackageNotFoundError
 
-try:
-    dist_name = 'lpagg'
-    # Try to get the version name from the installed package
-    __version__ = version(dist_name)
-except PackageNotFoundError:
-    try:
-        # If package is not installed, try to get version from git
-        from setuptools_scm import get_version
-        __version__ = get_version(version_scheme='post-release',
-                                  root='../..', relative_to=__file__)
-    except (LookupError, Exception):
-        __version__ = '0.0.0'
+__version__ = "0.20.0"
